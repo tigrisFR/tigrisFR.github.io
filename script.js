13d7d4ab24ee34bcdd44db42011d2d68e269ddcb
Adrien N <adrien.nabonne@gmail.com>
2024-02-28 11:37:39 -0800

    Add a quick and dirty chunked upload capability On click is bound by JS script for a change TODO: retries and perhaps use multipart FormaData.

--- script.js
+++ script.js
@@ -85,8 +85,14 @@ function login() {
 
 function uploadFile() {
     const fileInput = document.getElementById('fileInput');
+    const file = fileInput.files[0];
+    if (!file) {
+        console.log('No file selected');
+        alert('Error: No file selected');
+        return;
+    }
     const formData = new FormData();
-    formData.append('file', fileInput.files[0]);
+    formData.append('file', file);
 
     fetch(getBackendUrl() + '/upload', {
         method: 'POST',
@@ -99,6 +105,109 @@ function uploadFile() {
     .then(handleResponse)
     .then(data => alert(data.message))
     .catch(error => alert('Error: ' + error.message));
+}
+
+async function getResponseDataThrows(response) {
+    let data;
+    try {
+        data = await response.json();
+    } catch (error) {
+        console.log(`getResponseDataThrows: Could not parse Json`, response);
+        throw new Error(response.statusText);
+    }
+    if (!response.ok) {
+        console.log(`network response not ok`, data);
+        throw new Error(data.error);
+    }
+    return data;
+}
+
+document.getElementById('chunkedUploadButton').addEventListener('click', chunkedUploadFile);
+
+async function chunkedUploadFile() {
+    // This is very unsophisticated. We simply slice the file and upload
+    // the chunks sequentially.
+    // TODO: implement more sophisticated error recovery and retry mechanisms.
+    const fileInput = document.getElementById('fileInput');
+    const file = fileInput.files[0];
+    if (!file) {
+        console.log('No file selected');
+        alert('Error: No file selected');
+        return;
+    }
+    // get Chunked-Upload session from endpoint
+    let task_id;
+    try {
+        task_id = await negotiateChunkedUpload(file.name);
+    } catch (error) {
+        console.error('Failed to acquire chunked upload session', error);
+        alert('Error: ' + error.message);
+        return;
+    }
+
+    // update progressBar
+    const progressElement = document.getElementById('uploadProgress');
+    const progressBarZero = `▯`.repeat(10);
+    progressElement.innerText = `Upload progress: [${progressBarZero}] -init-`;
+    
+    // slice file in chunks
+    const chunkSize = 1024 * 1024; // 1MB
+    const totalChunks = Math.ceil(file.size / chunkSize);
+    
+    // vars to capture latest response details
+    var lastResponse = {};
+    var i = 0
+    try {
+        for (; i < totalChunks; i++) {
+            const startByte = i * chunkSize;
+            const endByte = Math.min(file.size, (i + 1) * chunkSize);
+            const chunk = file.slice(startByte, endByte);
+            const contentRange = `bytes ${startByte}-${endByte - 1}/${file.size}`;
+            
+            const response = await fetch(getBackendUrl() + '/upload', {
+                method: 'POST',
+                headers: {
+                    'Task-ID': task_id,
+                    'Content-Range': contentRange,
+                    'Authorization': 'Bearer ' + accessToken,
+                },
+                body: chunk,
+            });
+            lastResponse.status = response.status;
+            const data = await getResponseDataThrows(response);
+            lastResponse.data = data;
+            
+            //update visuals
+            const progress = (i + 1) / totalChunks;
+            const progressPercent = progress * 100;
+            const progressFloorOutOfTen = Math.floor(progress * 10);
+            const progressBar = `▮`.repeat(progressFloorOutOfTen) + `▯`.repeat(10 - progressFloorOutOfTen);
+            console.log(`Chunk ${i + 1}/${totalChunks}:`, data);
+            progressElement.innerText = `Upload progress: [${progressBar}] ${progressPercent.toFixed(2)}%`;
+        }
+        alert(lastResponse.data.message);
+        if (lastResponse.status == 202) { // Server is still expecting more chunks
+            throw new Error("Server did not ack end of upload despite receiving all chunks");
+        }
+    } catch (error) {
+        console.error('Upload failed for chunk', i + 1, error);
+        progressElement.innerText = 'Upload failed. See console for details.';
+        alert('Error: ' + error.message);
+    }
+}
+
+async function negotiateChunkedUpload(fileName) {
+    const response = await fetch(getBackendUrl() + '/chunked-upload-session', {
+        method: 'GET',
+        headers: {
+            'ngrok-skip-browser-warning': 'true',
+            'Authorization': 'Bearer ' + accessToken,
+            'Content-Disposition': `attachment; filename="${encodeURIComponent(fileName)}"`,
+        },
+    });
+    const data = await getResponseDataThrows(response);
+    console.log(`Chunked-upload session granted, task_id=${data.task_id}`);
+    return data.task_id;
 }
 
 function listFiles() {
